e2dc531487ddb8517498759e1c61768b872f011d
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-26 20:54:21 +0200

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,7 @@
+let colors = ["brown", "blue", "violet", "green"];
 
+let date = new Date();
+let minutes = date.getHours() * 60 + date.getMinutes();
+let choice = Math.floor(minutes / 15) % 4
+
+document.querySelector("header").style.backgroundColor = colors[choice]
