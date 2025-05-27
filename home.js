073bc88d94051f17c502224a7576a7dff8b1192a
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-27 06:31:41 +0200

    Update home.js

--- home.js
+++ home.js
@@ -6,4 +6,4 @@ let minutes = date.getHours() * 60 + date.getMinutes();
 let choice = Math.floor(minutes / 15) % 4
 
 document.querySelector("header").style.backgroundColor = colorsHeader[choice]
-document.querySelector("header").style.bottomBorder = colorsHeaderBorder[choice]
+document.querySelector("header").style.borderBottom = "5px solid ${colorsHeaderBorder[choice]}"
