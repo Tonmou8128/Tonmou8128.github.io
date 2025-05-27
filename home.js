587c489627656ef8c540248dceeca2f8ee021498
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-27 19:08:10 +0200

    rf

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ const colorsHeaderBorder = ["red", "black", "orange", "pink"]
 
 let date = new Date();
 let minutes = date.getHours() * 60 + date.getMinutes();
-let choice = Math.floor(minutes / 15) % 4
+let choice = Math.floor(minutes / 15) % 4;
 
-document.querySelector("header").style.backgroundColor = colorsHeader[choice]
+document.querySelector("header").style.backgroundColor = colorsHeader[choice];
 document.querySelector("header").style.borderBottom = "5px solid ${colorsHeaderBorder[choice]}"
