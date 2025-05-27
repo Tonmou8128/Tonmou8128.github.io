ead3ad86f30aefcf25323f8e0d9b2d51a0a13591
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-27 20:56:39 +0200

    Update home.js

--- scripts/home/home.js
+++ scripts/home/home.js
@@ -1,7 +1,6 @@
-const colorsHeader = ["#f3f55f", "#67d9e4", "#b567e4", "#36cf5f"]; // jaune - bleu - violet - vert
-const colorsHeaderBorder = ["#e2d22f", "#46c3cf", "#9d47d1", "#289946"] // pareil
-
-function headerColor(){
+export function headerColor(){
+    const colorsHeader = ["#f3f55f", "#67d9e4", "#b567e4", "#36cf5f"]; // jaune - bleu - violet - vert
+    const colorsHeaderBorder = ["#e2d22f", "#46c3cf", "#9d47d1", "#289946"] // pareil
     let date = new Date();
     let minutes = date.getHours() * 60 + date.getMinutes();
     let choice = Math.floor(minutes / 15) % 4;
