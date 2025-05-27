ead0bcb349af39e58fb65737bf4d19bd5be956d4
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-27 06:29:48 +0200

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
-let colors = ["brown", "blue", "violet", "green"];
+const colorsHeader = ["brown", "blue", "violet", "green"];
+const colorsHeaderBorder = ["red", "black", "orange", "pink"]
 
 let date = new Date();
 let minutes = date.getHours() * 60 + date.getMinutes();
 let choice = Math.floor(minutes / 15) % 4
 
-document.querySelector("header").style.backgroundColor = colors[choice]
+document.querySelector("header").style.backgroundColor = colorsHeader[choice]
+document.querySelector("header").style.bottomBorder = colorsHeaderBorder[choice]
