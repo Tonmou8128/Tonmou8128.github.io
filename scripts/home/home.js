27555748272ecc48193258eea8d0b9698bddd504
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-28 07:25:22 +0200

    Update home.js

--- scripts/home/home.js
+++ scripts/home/home.js
@@ -2,8 +2,7 @@ export function headerColor(){
     const colorsHeader = ["#f3f55f", "#67d9e4", "#b567e4", "#36cf5f"]; // jaune - bleu - violet - vert
     const colorsHeaderBorder = ["#e2d22f", "#46c3cf", "#9d47d1", "#289946"] // pareil
     let date = new Date();
-    let minutes = date.getHours() * 60 + date.getMinutes();
-    let choice = Math.floor(minutes / 15) % 4;
+    let choice = Math.floor(date.getMinutes() / 15);
 
     document.querySelector("header").style.backgroundColor = colorsHeader[choice];
     document.querySelector("header").style.borderBottom = "5px solid " + colorsHeaderBorder[choice]
