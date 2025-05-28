5788209a61ad9233e15b44a0ba01bbd73f0e3d90
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-05-28 17:09:31 +0200

    ds

--- scripts/home/home.js
+++ scripts/home/home.js
@@ -3,9 +3,17 @@ export function headerColor(){
     const colorsHeaderBorder = ["#e2d22f", "#46c3cf", "#9d47d1", "#289946"] // pareil
     let date = new Date();
     let choice = Math.floor(date.getMinutes() / 15);
-
     document.querySelector("header").style.backgroundColor = colorsHeader[choice];
     document.querySelector("header").style.borderBottom = "5px solid " + colorsHeaderBorder[choice]
 }
 
-headerColor()
+function iconColor(){
+    const colorsHeader = ["#f3f55f", "#67d9e4", "#b567e4", "#36cf5f"];
+    let date = new Date();
+    let choice = Math.floor(date.getMinutes() / 15);
+    document.querySelector(".icon").style.border = "5px solid " + colorsHeader[choice]
+}
+
+headerColor();
+
+iconColor();
