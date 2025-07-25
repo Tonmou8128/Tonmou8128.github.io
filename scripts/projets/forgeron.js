a54d34646396b4ea85b36212969ea9937c5526b4
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-07-25 16:54:20 +0200

    dsf

--- scripts/projets/forgeron.js
+++ scripts/projets/forgeron.js
@@ -1,3 +1,8 @@
+import * as JSZip from "./jszip.js"
+
+let itemsBp = [];
+let itemsRp = [];
+
 function uuid() {
   const template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
   const choicex = "0123456789abcdef".split("");
@@ -39,11 +44,55 @@ function forgeManifest(type) {
     return JSON.stringify(manifest, null, 2)
 }
 
-
-
-function forgeItem() {
+function addItem() {
     const itemName = document.getElementById("itemName").value;
     const itemId = document.getElementById("itemId").value;
     const itemIcon = document.getElementById("itemIcon").files[0];
-    const itemIconName = itemIcon.split(":")[1];
+    const itemIconName = itemId.split(":")[1];
+    const itemCategory = document.getElementById("itemCategory").value;
+    itemsBp.push([itemName, itemId, itemIconName, itemCategory]);
+    itemsRp.push(itemIcon);
+}
+
+function forgeItemJson([itemName, itemId, itemIcon, itemCategory]) {
+    const item = {
+        format_version: "1.21.90",
+        "minecraft:item": {
+            description: {
+                identifier: itemId,
+                menu_category: {
+                    category: itemCategory
+                }
+            },
+            components: {}
+        }
+    };
+    return JSON.stringify(item, null, 2);
+}
+
+function forgeItemLang(itemName, itemId) {
+    return `item.${itemId}=${itemName}`;
+}
+
+function forgeCompleteItemLang() {
+    let lang = ""
+    itemsBp.forEach(item => {
+        lang += forgeItemLang(item[0], item[1]) + "\n";
+    });
+    return lang;
+}
+
+function forgePack() {
+    let pack = new JSZip();
+    zip.file("BP/manifest.json", forgeManifest("data"));
+    zip.file("RP/manifest.json", forgeManifest("resources"));
+    itemsBp.forEach(item => {
+        zip.file(`BP/items/${item[1].split(":")[1]}.json`, forgeItemJson(item));
+        zip.file("BP/texts/fr_FR.lang", forgeCompleteItemLang());
+    });
+    itemsRp.forEach(item => {
+        const image = await item.arrayBuffer();
+        zip.file(`RP/textures/item/${itemsBp[itemsRp.indexOf(item)][1].split(":")[1]}.png`, image);
+        zip.file("RP/texts/fr_FR.lang", forgeCompleteItemLang());
+    });
 }
