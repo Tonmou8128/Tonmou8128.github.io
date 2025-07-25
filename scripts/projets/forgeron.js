dd4c26ac2131c706088886cf169da1726d835bb6
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-07-25 18:10:20 +0200

    cv

--- scripts/projets/forgeron.js
+++ scripts/projets/forgeron.js
@@ -113,5 +113,5 @@ async function forgePack() {
     })
 }
 
-document.querySelectorB("button#download").addEventListener("click", forgePack);
+document.querySelector("button#download").addEventListener("click", forgePack);
 document.querySelector("button#additem").addEventListener("click", addItem);
