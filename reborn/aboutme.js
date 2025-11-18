aa3381a6adeb24386160a9204d19bd7b79caa4e5
Tonmou8128 <149319443+Tonmou8128@users.noreply.github.com>
2025-11-18 19:01:50 +0100

    a lot

---
+++ reborn/aboutme.js
@@ -0,0 +1,22 @@
+const images = [...document.getElementsByClassName("icon")]
+const infos = [...document.getElementsByClassName("infos")]
+
+images.forEach(element => {
+    element.addEventListener("click", (event) => {
+        document.getElementById(element.id.replace("Icon", "").concat("Infos")).classList.add("visible");
+        images.forEach(element => {
+            element.classList.add("hidden");
+        });
+    });
+});
+
+infos.forEach(element => {
+    element.addEventListener("click", (event) => {
+        images.forEach(image => {
+            element.classList.remove("visible");
+            images.forEach(image => {
+                image.classList.remove("hidden");
+            });
+        });
+    });
+});
--- reborn/home.js
+++ reborn/home.js
@@ -1,12 +1,13 @@
 // ----------[ TITLE ]----------
 
-const words = ["un Giga Chad", "moi", "un professionnel", "un Être supérieur", "un fan de Shaka Ponk", "le meilleur dev de 2025", "un gars ordinaire"];
+const words = ["un Giga Chad", "moi", "un professionnel", "print('Tonmou8128')", "l'idole de tous", "un gars ordinaire", "un passionné", "un artiste", "un mec cool", "un beau gosse", "le dieu du code", "un gros sing- quoi ??"];
 
 const title = document.getElementById("magicTitle");
 
 title.addEventListener("mouseenter", (event) => {
     title.style.transition = "transform 0.1s ease-in-out";
     title.style.transform = `scale(${Math.random() * 0.5 + 1}) rotate(${Math.random() * 20 - 10}deg)`;
+    title.style.animation = "breathe 2s infinite ease-in-out";
     setTimeout(() => {
         title.textContent = words[Math.floor(Math.random() * words.length)];
     }, 150);
@@ -16,6 +17,7 @@ title.addEventListener("mouseenter", (event) => {
 title.addEventListener("mouseleave", (event) => {
     title.style.transition = "transform 0.1s ease-in-out";
     title.style.transform = "none";
+    title.style.animation = "none";
     setTimeout(() => {
         title.textContent = "Tonmou8128";
         title.style.transform = "rotateY(0deg)";
